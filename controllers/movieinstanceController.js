2d5cbd332e00584bcb7c168f391cebac4386f590
Jackson Wu <jacksonwu226@gmail.com>
2024-02-12 18:57:02 -0800

    added update GET/POST methods to movieinstanceController fixed a bug where update page does not load data from movieinstance added update link to movieinstance details page

--- controllers/movieinstanceController.js
+++ controllers/movieinstanceController.js
@@ -54,7 +54,7 @@ exports.movieinstance_create_post = [
     if(!errors.isEmpty()){
       const allMovies = await Movie.find({}, "title").sort({title: 1}).exec();
       res.render("movieinstance_form",{
-        titlie: "Create MovieInstance",
+        title: "Create MovieInstance",
         movie_list: allMovies,
         selected_movie: movieInstance.movie._id,
         errors: errors.array(),
@@ -83,10 +83,53 @@ exports.movieinstance_delete_post = asyncHandler(async(req, res, next) =>{
   res.redirect("/catalog/movieinstances")
 })
 
-exports.movieinstance_update_post = asyncHandler(async(req, res, next)=>{
-  res.send("NOT IMPLEMENTED: MOVIE INSTANC UPDATE POST");
+exports.movieinstance_update_get = asyncHandler(async(req, res, next)=>{
+  const [movieInstance, allMovies] = await Promise.all([
+    MovieInstance.findById(req.params.id).exec(),
+    Movie.find({},"title").sort({title:1}).exec()
+  ])
+  if(movieInstance === null){
+    const err = new Error("Movie instance not found");
+    err.status = 404;
+    return next(err);
+  }
+  res.render("movieinstance_form", {
+    title: "Update MovieInstance",
+    movie_list: allMovies,
+    selected_movie: movieInstance.movie._id,
+    movieinstance: movieInstance,
+  });
 });
 
-exports.movieinstance_update_get = asyncHandler(async(req,res,next) => {
-  res.send("NOT IMPLEMENTED: MOVIE INSTANCE UPDATE GET")
-})
+exports.movieinstance_update_post = [
+  body('movie', "Movie must be specified").trim().isLength({min: 1}).escape(),
+  body('status').escape(),
+  body('price')
+    .trim()
+    .isFloat({min:0})
+    .escape(),
+
+  asyncHandler(async(req,res,next)=>{
+    const errors = validationResult(req);
+    const movieInstance = new MovieInstance({
+      movie: req.body.movie,
+      status: req.body.status,
+      price: req.body.price,
+      _id: req.params.id
+    })
+    if(!errors.isEmpty()){
+      const allMovies = await Movie.find({}, "title").sort({title: 1}).exec();
+      res.render("movieinstance_form",{
+        title: "Create MovieInstance",
+        movie_list: allMovies,
+        selected_movie: movieInstance.movie._id,
+        errors: errors.array(),
+        movieinstance: movieInstance
+      })
+      return;
+    }else{
+      await MovieInstance.findByIdAndUpdate(req.params.id, movieInstance);
+      res.redirect(movieInstance.url);
+    }
+  }),
+];
