1ff5bf217e6e2a70662574a76da464b7fef35142
Jackson Wu <jacksonwu226@gmail.com>
2024-02-12 18:32:04 -0800

    added movieinstance form pug file added movieinsetance create get/post methods in controller file

--- controllers/movieinstanceController.js
+++ controllers/movieinstanceController.js
@@ -1,4 +1,7 @@
 const MovieInstance = require("../models/movieinstance");
+const Movie = require('../models/movie');
+
+const {body, validationResult} = require("express-validator")
 const asyncHandler = require("express-async-handler");
 
 // display all movie instances
@@ -26,13 +29,44 @@ exports.movieinstance_detail = asyncHandler(async (req,res, next)=> {
 });
 
 exports.movieinstance_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: MOVIE INSTANCE CREATE GET");
+  const allMovies = await Movie.find({}, "title").sort({title:1}).exec();
+  res.render("movieinstance_form", {
+    title: "Create Movie Instance",
+    movie_list: allMovies
+  })
 });
 
-exports.movieinstance_create_post = asyncHandler(async(req, res, next)=> {
-  res.send("NOT IMPLEMENTED: MOVIE INSTANCE CREATE POST");
-});
+exports.movieinstance_create_post = [
+  body('movie', "Movie must be specified").trim().isLength({min: 1}).escape(),
+  body('status').escape(),
+  body('price')
+    .trim()
+    .isFloat({min:0})
+    .escape(),
 
+  asyncHandler(async(req,res,next)=>{
+    const errors = validationResult(req);
+    const movieInstance = new MovieInstance({
+      movie: req.body.movie,
+      status: req.body.status,
+      price: req.body.price,
+    })
+    if(!errors.isEmpty()){
+      const allMovies = await Movie.find({}, "title").sort({title: 1}).exec();
+      res.render("movieinstance_form",{
+        titlie: "Create MovieInstance",
+        movie_list: allMovies,
+        selected_movie: movieInstance.movie._id,
+        errors: errors.array(),
+        movieinstance: movieInstance
+      })
+      return;
+    }else{
+      await movieInstance.save();
+      res.redirect(movieInstance.url);
+    }
+  }),
+];
 exports.movieinstance_delete_get = asyncHandler(async(req,res,next)=>{
   const movieinstance = await MovieInstance.findById(req.params.id).populate('movie').exec();
   if(movieinstance === null){
